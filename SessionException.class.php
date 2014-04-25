c5fdd7742698e8513a17ab3ced096fd81602e515
JayQ1982 <christof.moser@actra.ch>
2014-04-25 11:56:54 +0200

    Initial commit

---
+++ SessionException.class.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @author METANET <entwicklung@metanet.ch>
+ * @copyright Copyright (c) 2014, METANET AG
+ */
+
+namespace classes\session;
+
+class SessionException extends \Exception {
+
+}
+
+/* EOF */ 
